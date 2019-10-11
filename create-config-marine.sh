0f0a044d51b8f8a7edccf66a95d317c5a2e792b8
mirand863 <miranda.fmm@gmail.com>
2019-10-11 16:01:18 +0200

    Update create-config-marine.sh

--- create-config-marine.sh
+++ create-config-marine.sh
@@ -1,15 +1,15 @@
 
 #!/bin/bash
 
-echo "workdir: '/mnt/files/cami2/marine/'"
+echo "workdir: '/mnt/files/ganon/marine/'"
 echo ""
 echo "threads: 6"
 echo ""
 echo "samples:"
 for i in `seq 0 9`; do
-    echo "    fq$i: '/mnt/files/cami2/marine/reads/marmgCAMI2_short_read_sample_${i}_reads.fq.gz'"
+    echo "    fq$i: '/mnt/files/ganon/marine/reads/marmgCAMI2_short_read_sample_${i}_reads.fq.gz'"
 done
 echo ""
-echo "taxdump: '/mnt/files/cami2/ncbi_taxonomy/taxdump.tar.gz'"
+echo "taxdump: '/mnt/files/ganon/ncbi_taxonomy/taxdump.tar.gz'"
 echo ""
-echo "refseq_folder: '/mnt/files/cami2/refseq'"
+echo "refseq_folder: '/mnt/files/ganon/refseq'"
