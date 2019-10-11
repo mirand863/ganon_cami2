3955790d3906bb4c5ba503db672b79068fec6f77
mirand863 <miranda.fmm@gmail.com>
2019-10-11 14:41:27 +0200

    Added taxonomy database download

--- download-datasets.sh
+++ download-datasets.sh
@@ -16,3 +16,7 @@ java \
 "/mnt/files/cami2/strain-madness.linkfile" \
 /mnt/files/cami2/strain-madness \
 -p short
+
+wget \
+-O ncbi_taxonomy.tar \
+https://openstack.cebitec.uni-bielefeld.de:8080/swift/v1/CAMI_2_DATABASES/ncbi_taxonomy.tar
