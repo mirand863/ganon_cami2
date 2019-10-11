c45cf1f28786e55336c124493b369e5055a9ebb7
mirand863 <miranda.fmm@gmail.com>
2019-10-11 15:44:42 +0200

    Added commands to download refseq

--- download-datasets.sh
+++ download-datasets.sh
@@ -21,3 +21,11 @@ wget \
 -O ncbi_taxonomy.tar \
 https://openstack.cebitec.uni-bielefeld.de:8080/swift/v1/CAMI_2_DATABASES/ncbi_taxonomy.tar
 tar -xvf ncbi_taxonomy.tar
+
+wget \
+-O RefSeq_genomic_20190108.tar \
+https://openstack.cebitec.uni-bielefeld.de:8080/swift/v1/CAMI_2_DATABASES/RefSeq_genomic_20190108.tar
+mkdir refseq
+cd refseq/
+tar -xvf ../RefSeq_genomic_20190108.tar
+cd ..
