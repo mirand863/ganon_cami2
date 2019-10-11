33a048854cfb16cf5f448163b7f91d6a1a18b36f
mirand863 <miranda.fmm@gmail.com>
2019-10-11 14:59:02 +0200

    Fixed bug
    Would not print sample number before this fix

--- create-config-marine.sh
+++ create-config-marine.sh
@@ -7,7 +7,7 @@ echo "threads: 6"
 echo ""
 echo "samples:"
 for i in `seq 0 9`; do
-    echo "    fq$i: '/mnt/files/cami2/marine/reads/marmgCAMI2_short_read_sample_$i_reads.fq.gz'"
+    echo "    fq$i: '/mnt/files/cami2/marine/reads/marmgCAMI2_short_read_sample_${i}_reads.fq.gz'"
 done
 echo ""
 echo "taxdump: '/mnt/files/cami2/ncbi_taxonomy/taxdump.tar.gz'"
